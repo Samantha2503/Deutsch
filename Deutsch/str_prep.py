6a67d222f75321416053bc268ecd8d39f7f4eb0f
Samantha2503 <44532747+Samantha2503@users.noreply.github.com>
2025-08-25 23:18:30 -0600

    .

--- Deutsch/str_prep.py
+++ Deutsch/str_prep.py
@@ -149,22 +149,10 @@ if st.button("Submit") and not st.session_state.submitted:
         )
     st.info(f"Translation: {st.session_state.current_q['translation']}")
 
-# if "current_q" not in st.session_state:
-#      st.session_state.current_q = random.choice(verbs)
-#if "submitted" not in st.session_state: 
-#      st.session_state.submitted = False
-    
 if st.button("Next Question"):
     st.session_state.current_q = random.choice(verbs)
     st.session_state.submitted = False
-    #st.stop()
-
-
-
-
-
-
-    
+    st.experimental_rerun()
 
 st.write(f"Score: {st.session_state.score}")
 if st.session_state.wrong_answers:
